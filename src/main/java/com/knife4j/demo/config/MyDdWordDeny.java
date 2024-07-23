5755697102a8b597dc8702d544ff9d1d26a83f1f
hss-oss <176091488+hss-oss@users.noreply.github.com>
2024-07-23 14:13:07 +0800

    读取txt文本改成IoUtils

--- src/main/java/com/knife4j/demo/config/MyDdWordDeny.java
+++ src/main/java/com/knife4j/demo/config/MyDdWordDeny.java
@@ -4,7 +4,7 @@
 package com.knife4j.demo.config;
 
 import com.github.houbb.sensitive.word.api.IWordDeny;
-import org.springframework.stereotype.Component;
+import com.knife4j.demo.util.IoUtils;
 import java.util.List;
 
 /**
@@ -12,12 +12,11 @@ import java.util.List;
  * @author yuwei
  * @version $Id: MyDdWordDeny.java, v 0.1 2024年07月19日 9:55 PM yuwei Exp $
  */
-@Component
-public class MyDdWordDeny extends LocalDictionary implements IWordDeny {
+public class MyDdWordDeny implements IWordDeny {
 
 
     @Override
     public List<String> deny() {
-        return readWords("敏感词库.txt");
+        return IoUtils.readWords("敏感词库.txt");
     }
 }
--- src/main/java/com/knife4j/demo/util/IoUtils.java
+++ src/main/java/com/knife4j/demo/util/IoUtils.java
@@ -1,35 +1,39 @@
 /**
  * Alipay.com Inc. Copyright (c) 2004-2024 All Rights Reserved.
  */
-package com.knife4j.demo.config;
+package com.knife4j.demo.util;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 本地词库
+ * IO流工具类
  *
  * @author yuwei
  * @version $Id: LocalDictionary.java, v 0.1 2024年07月20日 10:22 PM yuwei Exp $
  */
-public class LocalDictionary {
+public class IoUtils {
 
     /**
      * 读取词库
      *
      * @return
      */
-    protected List<String> readWords(String fileName) {
+    public static List<String> readWords(String fileName) {
         String rootPath = System.getProperty("user.dir");
         String filePath = rootPath + File.separator + fileName;
         System.out.println("=============文件地址" + filePath);
         List<String> words = new ArrayList<>();
         try {
-            BufferedReader br = new BufferedReader(new FileReader(filePath));
+            InputStream in =new FileInputStream(filePath);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, Charset.forName("UTF-8")));
             String line;
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
